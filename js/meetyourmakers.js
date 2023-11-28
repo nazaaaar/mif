e4ca7a54f13f2b52afce98e39e8db0d6f07f136d
ferax3 <denis.borovikov.2004@gmail.com>
2023-11-28 11:17:34 +0200

    SUMMARY 107

--- js/meetyourmakers.js
+++ js/meetyourmakers.js
@@ -32,7 +32,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function isWeekend(date) {
     // Get the day of the week (0 = Sunday, 1 = Monday, ..., 6 = Saturday)
-    var dayOfWeek = date.getDay();
+    let dayOfWeek = date.getDay();
 
     // Check if the day is Sunday (0) or Saturday (6)
     return dayOfWeek === 0 || dayOfWeek === 6;
@@ -45,11 +45,35 @@ function updateGodServices() {
     dropdown2.innerHTML = "";
 
     if (dropdown1Value === "Перун") {
-        dropdown2.options.add(new Option("Option A", "6"));
-        dropdown2.options.add(new Option("Option B", "4"));
+        dropdown2.options.add(new Option("Виклик грому та блискавки", "15"));
+        dropdown2.options.add(new Option("Захист від стихійних лих", "25"));
     } else if (dropdown1Value === "Дажбог") {
-        dropdown2.options.add(new Option("Option X", "7"));
-        dropdown2.options.add(new Option("Option Y", "4"));
+        dropdown2.options.add(new Option("Сонячна енергія для зарядки", "10"));
+        dropdown2.options.add(new Option("Організація яскравого сонячного дня", "30"));
+    } else if (dropdown1Value === "Стрибог") {
+        dropdown2.options.add(new Option("Сприяння веснянім вітрам", "12"));
+        dropdown2.options.add(new Option("Збільшення швидкості вітру", "20"));
+    } else if (dropdown1Value === "Сварог") {
+        dropdown2.options.add(new Option("Запуск вогненного шоу", "18"));
+        dropdown2.options.add(new Option("Виготовлення вогненної зброї або інструментів", "35"));
+    } else if (dropdown1Value === "Велес") {
+        dropdown2.options.add(new Option("Покращення родючості землі", "22"));
+        dropdown2.options.add(new Option("Поради щодо розвитку сільськогосподарських справ", "12"));
+    } else if (dropdown1Value === "Мокоша") {
+        dropdown2.options.add(new Option("Благословення на здоров'я та родючість", "28"));
+        dropdown2.options.add(new Option("Наведення порядку в домашньому господарстві", "15"));
+    } else if (dropdown1Value === "Чорнобог та Білобог") {
+        dropdown2.options.add(new Option("Захист від негативних впливів", "20"));
+        dropdown2.options.add(new Option("Допомога в досягненні балансу в житті", "40"));
+    } else if (dropdown1Value === "Ярило") {
+        dropdown2.options.add(new Option("Подарунок весняного тепла та світла", "15"));
+        dropdown2.options.add(new Option("Підтримка весняних фестивалів та заходів", "25"));
+    } else if (dropdown1Value === "Мара") {
+        dropdown2.options.add(new Option("Захист від негоди та повеней", "18"));
+        dropdown2.options.add(new Option("Допомога у з'явленні джерела чистої води", "30"));
+    } else if (dropdown1Value === "Леля") {
+        dropdown2.options.add(new Option("Поради та підтримка в коханні та стосунках", "15"));
+        dropdown2.options.add(new Option("Організація романтичного свята для двох", "25"));
     }
 }
 
@@ -61,19 +85,42 @@ function updatePrice(){
     const selectedOption = dropdown2.options[dropdown2.selectedIndex];
     let price = parseInt(selectedOption.value);
 
-    if (isWeekend(datePicker.value)){
-        price*=1.2;
+
+    if (isWeekend(new Date(datePicker.value))){
+        price*=1.4;
         price = Math.round(price);
     }
 
     if (price===1){
-        priceText.innerText="Дана послуга вам буде коштувати"+price+" молитву."
+        priceText.innerText="Дана послуга вам буде коштувати "+price+" молитву."
     }
     else if( price>1 && price<5){
-        priceText.innerText="Дана послуга вам буде коштувати"+price+" молитви."
+        priceText.innerText="Дана послуга вам буде коштувати "+price+" молитви."
     }
     else{
-        priceText.innerText="Дана послуга вам буде коштувати"+price+" молитв."
+        priceText.innerText="Дана послуга вам буде коштувати "+price+" молитв."
     }
 
+}
+
+function toggleTable() {
+    const table = document.getElementById("godsTable");
+    const button = document.getElementById("gods-table-button")
+    if (table.style.display === "none" || table.style.display === "") {
+        table.style.display = "table"; // Show the table
+        button.innerText="Сховати";
+    } else {
+        table.style.display = "none"; // Hide the table
+        button.innerText="Дізнатися більше про богів";
+    }
+}
+
+function submitForm() {
+    const form = document.getElementById("myForm");
+    const successMessage = document.getElementById("successMessage");
+
+    form.style.display = 'none';
+
+    successMessage.innerHTML = "Вашу форму відправлено";
+    successMessage.style.display = 'block';
 }
